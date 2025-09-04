48198ee08db8a5a2e1421f10f38bc6303b6d0097
sawa-ko <56084970+sawa-ko@users.noreply.github.com>
2025-09-04 16:12:42 -0600

    feat: add struct for event metadata handling and management

---
+++ src/event_metadata.rs
@@ -0,0 +1,29 @@
+use chrono::{DateTime, Utc};
+use uuid::Uuid;
+
+use crate::Event;
+
+#[derive(Debug, Clone)]
+pub struct EventMetadata {
+    pub event_id: Uuid,
+    pub timestamp: DateTime<Utc>,
+    pub correlation_id: Uuid,
+    pub causation_id: Option<Uuid>,
+}
+
+impl EventMetadata {
+    pub fn new(correlation_id: Uuid, causation_id: Option<Uuid>) -> Self {
+        Self {
+            event_id: Uuid::new_v4(),
+            timestamp: Utc::now(),
+            correlation_id,
+            causation_id,
+        }
+    }
+}
+
+#[derive(Debug, Clone)]
+pub struct EventEnvelope<E: Event> {
+    pub event: E,
+    pub metadata: EventMetadata,
+}
